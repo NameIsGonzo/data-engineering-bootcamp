ae18045bb54c0854451ac9e27c769431b682f4cd
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-01-22 15:33:36 -0600

    add terraform file with project variables

---
+++ week_1/cloud/variables.tf
@@ -0,0 +1,24 @@
+locals {
+  data_lake_bucket = "dtc_data_lake"
+}
+
+variable "project" {
+    description = "dtc-data-eng-course"
+}
+
+variable "region" {
+  description = "Region for GCP Resources"
+  default = "us-wet1"
+  type = string
+}
+
+variable "storage_class" {
+  description = "Storage class type for our buckets"
+  default = "STANDARD"
+}
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data from GCS will be written to"
+  type = string
+  default = "trips_data_all"
+}
