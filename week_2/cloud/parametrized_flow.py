f4d8b15699961d3d812753577602dab4b0631a83
GonzaloAlcalaGD <galcala@griddynamics.com>
2023-02-16 10:46:25 -0600

    add schema enforcement feature for parquet files

--- week_2/cloud/parametrized_flow.py
+++ week_2/cloud/parametrized_flow.py
@@ -1,22 +1,13 @@
 from pathlib import Path
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+import pyarrow as pa
+import pyarrow.parquet as pq
+import pyarrow.compute as pc
 import pandas as pd
 import os
 import sys
-
-
-@task(log_prints=True)
-def filename_from_url(url: str) -> str:
-    """Extract filename from url that downloads a .parquet file"""
-    try:
-        output = url.split('/')
-        output = output[4].replace('.parquet', '').replace('-', '_')
-        return output
-    except ValueError as e:
-        print('The url doesnt have the correct format')
-        print(e)
-        return sys.exit(1)
+import yaml
 
 
 def get_subdir(filename: str) -> str:
@@ -29,20 +20,50 @@ def get_subdir(filename: str) -> str:
         return sys.exit(1)
 
 
-@task()
+@task(log_prints=True)
+def enforce_schema(df: pd.DataFrame, color: str, dir='/opt/prefect/schemas/data_schemas.yaml') -> pd.DataFrame:
+
+    with open(dir, 'rb') as f:
+        color_schema = yaml.safe_load(f)[color]
+    print(f'Working with schema for dataset {color}')
+    print(color_schema)
+    enforced_df = df.astype(color_schema)
+    if color == 'yellow':
+        enforced_df = enforced_df.drop(columns=['airport_fee'])
+    return enforced_df
+
+
+@task(log_prints=True)
+def filter_out_of_bounds(directory: str) -> pd.DataFrame:
+    """Filters out of bounds records from parquet files."""
+    table = pq.read_table(directory)
+    try:
+        table = table.filter(
+            pc.less_equal(table['dropOff_datetime'], pa.scalar(pd.Timestamp.max))
+        )
+        filter_df = table.filter(
+            pc.less_equal(table['pickup_datetime'], pa.scalar(pd.Timestamp.max))
+        ).to_pandas()
+        return filter_df
+    except:
+        print('Field not found, skipping filter')
+        return table.to_pandas()
+
+
+@task(log_prints=True)
 def extract_data(url: str, file_name: str, color: str) -> pd.DataFrame:
     """Downloads a parquet file and returns it as a pandas Dataframe"""
+    file_dir = f'dataset/{color}/{file_name}.parquet'
     try:
-        os.system(f'curl {url} -o dataset/{color}/{file_name}.parquet')
-        df = pd.read_parquet(f'dataset/{color}/{file_name}.parquet', engine='pyarrow')
-        return df
+        os.system(f'curl {url} -o {file_dir}')
+        return
     except ValueError as e:
         print(e)
         return sys.exit(1)
 
 
-@task()
-def write_gcs(path: Path, filename: str):
+@task(log_prints=True)
+def write_gcs(path: Path, filename: str) -> None:
     """Loads a GCS block with our credentials and uploads the file to our GCS Bucket"""
     gcs_path = f'{get_subdir(filename)}/{filename}.parquet'
     gcs_block = GcsBucket.load("datatalks-bootcamp")
@@ -50,13 +71,23 @@ def write_gcs(path: Path, filename: str):
     return
 
 
+@task(log_prints=True)
+def save_df(df: pd.DataFrame, directory: str)-> None:
+    df.to_parquet(directory, engine='pyarrow', compression='snappy')
+    return
+
+
 @flow()
 def etl_web_to_gcs(year: int, month: int, color: str) -> None:
     """Main ETL flow"""
     url = f'https://d37ci6vzurychx.cloudfront.net/trip-data/{color}_tripdata_{year}-{month:02}.parquet'
-    file_name = filename_from_url(url)
-    df = extract_data(url, file_name, color)
+    file_name = f'{color}_tripdata_{year}-{month:02}'
     path = f'dataset/{color}/{file_name}.parquet'
+
+    extract_data(url, file_name, color)
+    df = filter_out_of_bounds(path)
+    enforced_df = enforce_schema(df, color)
+    save_df(enforced_df, path)
     write_gcs(path, file_name)
 
 
